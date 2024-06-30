b6dd5ccdd20858d17d1c38bd103f140c45c97f8c
karthik <karthiknormie@gmail.com>
2024-06-30 20:54:35 +0530

    route structure

--- main.js
+++ main.js
@@ -7,6 +7,20 @@ app.get('/', (req,res) => {
     res.json({msg: "Hello students"});
 })
 
+app.get('/movies', () => {
+   
+})
+app.post('/movies', () => {
+   
+})
+app.put('/movies/:id', () => {
+   
+})
+app.delete('/movies/:id', (req,res) => {
+   
+})
+
+
 app.listen(6969, () => {
     console.log(`the server is running at http://localhost:${PORT}`);
 });
